c8799ccb69cb641176d283206ea4e8c1fe6f77a5
Ditamin <tagirovdinar1@gmail.com>
2023-11-20 15:34:11 +0500

    hw6 v1.0

--- src/test/java/edu/hw6/Task3Test.java
+++ src/test/java/edu/hw6/Task3Test.java
@@ -8,6 +8,7 @@ import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import static edu.hw6.Task3.IS_EXECUTABLE;
 import static edu.hw6.Task3.globMatches;
@@ -42,7 +43,7 @@ class Task3Test {
         Path path = Path.of("src/test/java/edu/hw6/filesForTests");
         ArrayList<Path> ans = new ArrayList<>(List.of(Path.of("HelloWorld.java"), Path.of("photo.PNG")));
         ArrayList<Path> res = new ArrayList<>();
-        DirectoryStream.Filter<Path> filter = IS_READABLE.and(IS_WRITABLE).and(IS_EXECUTABLE).and(IS_REGULAR_FILE);
+        DirectoryStream.Filter<Path> filter = IS_READABLE.and(IS_WRITABLE).and(IS_EXECUTABLE);
 
         try (DirectoryStream<Path> entries = Files.newDirectoryStream(path, filter)) {
             entries.forEach(entry -> res.add(entry.getFileName()));
@@ -72,11 +73,12 @@ class Task3Test {
     @Test
     void regexFilterTest() throws IOException {
         Path path = Path.of("src/test/java/edu/hw6/filesForTests");
-        ArrayList<Path> ans = new ArrayList<>(List.of(
+        HashSet<Path> ans = new HashSet<>(List.of(
             Path.of("photo.PNG"),
-            Path.of("text.txt")));
+            Path.of("text.txt")
+        ));
 
-        ArrayList<Path> res = new ArrayList<>();
+        HashSet<Path> res = new HashSet<>();
         DirectoryStream.Filter<Path> filter = regexContains("[t]");
 
         try (DirectoryStream<Path> entries = Files.newDirectoryStream(path, filter)) {
